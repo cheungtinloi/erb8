86d2b983956abc661e4e1db1d1124bbb31498d1f
cheungtinloi <cheungtinloi@gmail.com>
2025-12-24 14:36:05 +0800

    step54 listings & doctors model

--- config/settings.py
+++ config/settings.py
@@ -84,8 +84,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'clinic1',
+        'USER' : 'postgres',
+        'PASSWORD' : '1234',
+        'HOST' : 'localhost',
     }
 }
 
